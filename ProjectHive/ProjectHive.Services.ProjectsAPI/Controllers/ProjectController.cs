a4d49050017580bf0f2512a43e1eef6e25ff5f3b
marla124 <anyavoloschcooo@gmail.com>
2024-03-17 00:19:47 +0300

    create controllers

---
+++ ProjectHive/ProjectHive.Services.ProjectsAPI/Controllers/ProjectController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectHive.Services.ProjectsAPI.Controllers
+{
+    public class ProjectController : Controller
+    {
+        
+    }
+}
