c745f0dac643fc401a2e45dcd413fa5fec3bb02e
vadim-m-bt <152855634+vadim-m-bt@users.noreply.github.com>
2024-04-06 16:17:59 +0200

    Fix automapper

--- ProjectHive/ProjectHive.Services.ProjectsAPI/ProjectServiceCollectionExtention.cs
+++ ProjectHive/ProjectHive.Services.ProjectsAPI/ProjectServiceCollectionExtention.cs
@@ -1,10 +1,10 @@
-﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ProjectHive.Services.ProjectsAPI.Business.Services;
 using ProjectHive.Services.ProjectsAPI.Data;
 using ProjectHive.Services.ProjectsAPI.Data.Entities;
 using ProjectHive.Services.ProjectsAPI.Data.Repository;
 using ProjectHive.Services.ProjectsAPI.Data.Repository.Interfase;
+using System.Reflection;
 
 namespace ProjectHive.Services.ProjectsAPI
 {
@@ -15,9 +15,9 @@ namespace ProjectHive.Services.ProjectsAPI
         {
             var connectionString = configuration.GetConnectionString("Default");
             services.AddDbContext<ProjectHiveProjectDbContext>(opt => opt.UseNpgsql(connectionString));
-            var mapperConfig = Mapping.RegisterMaps();
-            IMapper mapper = mapperConfig.CreateMapper();
-            services.AddSingleton(mapper); //It doesn't work without it
+
+            services.AddAutoMapper(typeof(Mapping).GetTypeInfo().Assembly);
+
             services.AddScoped<IUnitOfWork<Project>, UnitOfWork<Project>>();
             services.AddScoped<IRepository<Project>, Repository<Project>>();
             services.AddScoped<IRepository<ProjectTask>, Repository<ProjectTask>>();
