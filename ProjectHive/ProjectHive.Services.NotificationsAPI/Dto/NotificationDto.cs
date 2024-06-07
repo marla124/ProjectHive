4e032c2909e277ca86f7beeeca500cc082409512
Волошко Марина <95192268+marla124@users.noreply.github.com>
2024-06-07 16:42:46 +0300

    add entities and dto

---
+++ ProjectHive/ProjectHive.Services.NotificationsAPI/Dto/NotificationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectHive.Services.NotificationsAPI.Dto
+{
+    public class NotificationDto
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public Guid UserId { get; set; }
+        public Guid NotificationStatusId { get; set; }
+    }
+}
---
+++ ProjectHive/ProjectHive.Services.NotificationsAPI/Extensions/AppBuilderExtensionStatusNotification.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectHive.Services.NotificationsAPI.Data;
+using ProjectHive.Services.NotificationsAPI.Data.Entities;
+
+namespace ProjectHive.Services.ProjectAPI.Extensions;
+
+public static class AppBuilderExtensionStatusNotification
+{
+    public static async Task<IApplicationBuilder> PrepareDatabaseNotification(this IApplicationBuilder app)
+    {
+        using var scopedServices = app.ApplicationServices.CreateScope();
+
+        var serviceProvider = scopedServices.ServiceProvider;
+        var dbContext = serviceProvider.GetRequiredService<ProjectHiveNotificationsDbContext>();
+
+        if (!await dbContext.NotificationStatuses.AnyAsync())
+        {
+            var status = new List<NotificationStatus>
+            {
+                new NotificationStatus { Name = "Sent" },
+                new NotificationStatus { Name = "Delivered" },
+                new NotificationStatus { Name = "Read" },
+                new NotificationStatus { Name = "Error" }
+            };
+
+            await dbContext.NotificationStatuses.AddRangeAsync(status);
+            await dbContext.SaveChangesAsync();
+        }
+
+        return app;
+    }
+}
