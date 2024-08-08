25621104f5e51e02c097eb1d7f0a02d278f6db86
Волошко Марина <95192268+marla124@users.noreply.github.com>
2024-08-09 01:15:11 +0300

    continue work

--- ProjectHive/ProjectHive.Services.NotificationsAPI/Controllers/NotificationController.cs
+++ ProjectHive/ProjectHive.Services.NotificationsAPI/Controllers/NotificationController.cs
@@ -1,27 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using ProjectHive.Services.NotificationsAPI.Handlers;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ProjectHive.Services.NotificationsAPI.Dto;
+using ProjectHive.Services.NotificationsAPI.Models;
+using ProjectHive.Services.NotificationsAPI.Services;
+using System.Net.WebSockets;
 
 namespace ProjectHive.Services.NotificationsAPI.Controllers
 {
     public class NotificationController : Controller
     {
-        private readonly WebSocketHandler _webSocketHandler;
-        public NotificationController()
+        private readonly IWebSocketHandler _webSocketHandler;
+        private readonly IMapper _mapper;
+        public NotificationController(IMapper mapper)
         {
             _webSocketHandler = new WebSocketHandler();
+            _mapper = mapper;
         }
-        [HttpGet("/ws")]
-        public async Task Get()
+        [HttpPost]
+        public async Task SendNotification(NotificationModel notification)
         {
-            if (HttpContext.WebSockets.IsWebSocketRequest)
+            await _webSocketHandler.SendNotificationAsync(_mapper.Map<NotificationDto>(notification));
+            return;
+        }
+        private async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            while (!result.CloseStatus.HasValue)
             {
-                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
-                await _webSocketHandler.Echo(webSocket);
+                await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+                result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
             }
-            else
-            {
-                HttpContext.Response.StatusCode = 400;
-            }
+            await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
         }
     }
 }
---
+++ ProjectHive/ProjectHive.Services.NotificationsAPI/Services/WebSocketHandler.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using ProjectHive.Services.NotificationsAPI.Dto;
+using ProjectHive.Services.NotificationsAPI.Models;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace ProjectHive.Services.NotificationsAPI.Services
+{
+    public class WebSocketHandler :IWebSocketHandler
+    {
+        private readonly List<WebSocket> _sockets = new List<WebSocket>();
+
+        public async Task SendNotificationAsync(NotificationDto notification)
+        {
+            var message = JsonConvert.SerializeObject(notification);
+            var buffer = Encoding.UTF8.GetBytes(message);
+            var segment = new ArraySegment<byte>(buffer);
+
+            foreach (var socket in _sockets)
+            {
+                if (socket.State == WebSocketState.Open)
+                {
+                    await socket.SendAsync(segment, WebSocketMessageType.Text, true, CancellationToken.None);
+                }
+            }
+        }
+
+        public void AddSocket(WebSocket socket)
+        {
+            _sockets.Add(socket);
+        }
+    }
+}
